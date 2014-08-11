014df29ca124b8b7d3d8f3d2a93307db6b4ddba0
Vince <lee790909@gmail.com>
2014-08-11 14:13:15 +0800

    hello world

---
+++ hello.py
@@ -0,0 +1,13 @@
+import os
+from flask import *
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def hello():
+    return render_template('template.html')
+
+if __name__ == '__main__':
+	app.run(port = 5000, debug=True)
